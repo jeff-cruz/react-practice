da6aa7bb5cec659fd72de31ae5746d484a6c4415
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-07-16 22:10:33 +0000

    React Hooks Checkbox

---
+++ my-app/src/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+
+function App() {
+  const [checked, setChecked] = useState(false);
+  return(
+    <div>
+      <input type="checkbox" value={checked} onChange={() => setChecked(!checked)} />
+      <p>{checked ? "checked" : "not checked"}</p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
