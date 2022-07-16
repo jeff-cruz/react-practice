d36d052278ce562b0f2df45233d1a08444e0534e
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-07-16 22:33:11 +0000

    React Hooks Star Input

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,16 +2,38 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { FaStar } from "react-icons/fa"
 
+const createArray = (length) => [
+  ...Array(length)
+];
+
+function Star({selected = false, onSelect }) {
+  return (
+    <FaStar color={selected ? "red" : "gray"} onClick={onSelect}/>
+  );
+}
+
+function StarRating({ totalStars = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+
+
+  return (
+  <>
+    {createArray(totalStars).map((n, i) => (
+      <Star
+        key={i}
+        selected={selectedStars > i}
+        onSelect={() => setSelectedStars(i + 1)}
+      />
+    ))}
+    <p>{selectedStars} of {totalStars}</p>
+  </>
+  );
+}
 
 function App() {
-  const [checked, setChecked] = useState(false);
-  return(
-    <div>
-      <input type="checkbox" value={checked} onChange={() => setChecked(!checked)} />
-      <p>{checked ? "checked" : "not checked"}</p>
-    </div>
-  )
+  return <StarRating totalStars={5}/>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
