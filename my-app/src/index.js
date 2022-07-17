49752a7c3a3248aa9dde0e6a5cd43fb451caeb84
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-07-17 23:47:00 +0000

    Fetch Hook for data fetching

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,18 +4,9 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
-const TreesContext = createContext();
-
-export const useTrees = () => useContext(TreesContext);
-
-const trees = [
-  { id: "1", type: "Maple" },
-  { id: "2", type: "Oak" },
-  { id: "3", type: "Family" },
-  { id: "4", type: "Fake" },
-  { id: "5", type: "Component" }
-]
-
+// if the data isn't available yet but is loading
+// if we get the data
+// if there's an error
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
