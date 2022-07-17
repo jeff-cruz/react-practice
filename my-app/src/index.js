25a9526161d6cd8615deda47fc033001161f26e0
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-07-17 23:56:30 +0000

    Building a fetch component EXCITING

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,33 @@
-import React, { createContext, useContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+import { useFetch } from './useFetch';
 
-// if the data isn't available yet but is loading
-// if we get the data
-// if there's an error
+function App({login}) {
+  const {loading, data, error} = useFetch(
+    `https://api.github.com/users/${login}`
+    );
+    if(loading) return <h1>loading...</h1>;
+    if(error)
+    return (
+      <pre>{JSON.stringify(error, null, 2)}</pre>
+    );
+    return (
+      <div>
+        <img src={data.avatar_url} alt={data.login}></img>
+        <div>
+          <h1>{data.login}</h1>
+          {data.name && <p>{data.name}</p>}
+          {data.location && <p>{data.location}</p>}
+        </div>
+      </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <TreesContext.Provider value = {{ trees }}>
-    <App />
-  </TreesContext.Provider>
+    <App login="jeff-cruz" />
 );
 
 // If you want to start measuring performance in your app, pass a function
