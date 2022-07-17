3e9303351deb365a9a3b098c63dd0d9f11ec2393
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-07-17 00:04:22 +0000

    useReducer for checkbox functionality

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,8 +5,17 @@ import reportWebVitals from './reportWebVitals';
 
 
 function App() {
-  const [number, setNumber] = useReducer((number, newNumber) => number + newNumber, 0);
-  return <h1 onClick={() => setNumber(1)}>{number}</h1>
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+  );
+
+  return (
+    <>
+      <input type="checkbox" value={checked} onChange={toggle} />
+      {checked ? "checked" : "not checked"}
+    </>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
